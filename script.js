11a0d8b24f92b7d87359331c954d7c1d93559dfd
gujejianig <gujejiani.websmart@gmail.com>
2022-03-09 16:10:55 +0400

    added edit btn

--- script.js
+++ script.js
@@ -1,50 +1,63 @@
 const addTaskInput = document.getElementById("add-task-input");
 const taskList = document.getElementById("tasks-list");
-const addTaskBtn = document.getElementById("add-task-button");
-const todoInput = document.querySelector('.todoInput')
 const paginationBtnWrapper = document.getElementById(
     "pagination-buttons-wrapper"
 );
 
+let  getIndex;
+
+
+
+
 let initialTaskData = [];
+
 let pagesData = [];
 
 function showTask() {
+    console.log(initialTaskData)
+    const innerDiv = document.createElement('div');
+    innerDiv.addEventListener('click', () =>  {});
+
     taskList.innerHTML =
         "<div " +
-
         pagesData[pagesData.length - 1]
-            ?.map((task) => {
-                return "<div class='task' >" + '<input readonly  class="todoInput" value=' + task.task + ' />' + "<button onclick=removeBtn(" + task.id + ") type='button' class='btn btn-danger'>" + 'Remove' + '</button>' + "</>";
+            ?.map((task, index) => {
+                getIndex = index
+                return "<div id=" + 'mainDiv' + task.id + " class='task' >" + '<p  id=' + 'changedValue' + task.id + ' class="todoInput"> ' + task.task + ' </p>' +
+                    "<input onclick=onCheckBox(" + task.id + ") id=" + 'checkBox' + task.id + " type='checkbox' />" +
+                    '<input value=' + task.task + ' id=' + 'ident' + task.id + ' class="hideItem" ' + '/>' +
+                    "<button onclick=removeBtn(" + task.id + ") type='button' class='btn btn-danger'>" + 'Remove' + '</button>'
+                    + "<button onclick=editHandler(" + task.id +  ")  type='button' id=" + 'edit' + task.id + " class='btn btn-info'>" + 'Edit' + "</button>" + "</div>"
             })
             .join("") +
         "</div>";
 }
 
 const addTask = () => {
+
     if (addTaskInput.value.trim().length !== 0) {
         initialTaskData.push({task: addTaskInput.value, id: Date.now()});
         addTaskInput.value = "";
 
 
         renderPagesData(initialTaskData)
-
-
     }
-
 };
 
 
 const changePage = (pageNumber) => {
     taskList.innerHTML = pagesData[pageNumber]
-        .map((pageList) => {
-
-            return "<div class='task'>" + '<input value=' + pageList.task + ' class="todoInput"/>' + "<button onclick=removeBtn(" + pageList.id +
-                ") type='button' class='btn btn-danger'>" + 'Remove' + '</button>' + "</div>";
+        .map((task, index) => {
+            return "<div id=" + 'mainDiv' + task.id + " class='task' >" + '<p id=' + 'changedValue' + task.id + ' class="todoInput"> ' + task.task + ' </p>' +
+                "<input onclick=onCheckBox(" + task.id + ") id=" + 'checkBox' + task.id + " type='checkbox' />" +
+                '<input value=' + task.task + ' id=' + 'ident' + task.id + ' class="hideItem" ' + '/>' +    // მჭირდება ეს ველიუ
+                "<button onclick=removeBtn(" + task.id + ") type='button' class='btn btn-danger'>" + 'Remove' + '</button>'
+                + "<button onclick=editHandler(" + task.id + ")  type='button' id=" + 'edit' + task.id + " class='btn btn-info'>" + 'Edit' + '</button>' + "</div>"
         })
         .join("");
-
 };
+
+
 
 addTaskInput.addEventListener("keyup", (e) => {
     if (e.keyCode === 13) {
@@ -58,6 +71,7 @@ const renderPagesData = (initialData) => {
     let counter = 0;
     pagesData = [];
     initialData.forEach((task, index) => {
+
         counter++;
         singlePageData.push(task);
         if (counter === 5 || index === initialData.length - 1) {
@@ -71,10 +85,11 @@ const renderPagesData = (initialData) => {
 
     showTask()
 
+
     paginationBtnWrapper.innerHTML = pagesData
         .map((singlePageList, index) => {
             return (
-                "<button class='btn btn-default' onclick=changePage(" + ////////////////////////////////////////
+                "<button class='btn btn-default' onclick=changePage(" +
                 index +
                 ")>" +
                 (index + 1) +
@@ -85,19 +100,56 @@ const renderPagesData = (initialData) => {
 
 }
 
-// Edit function should start here!
+// Item Editing
 
+const editHandler = (identifier, index) => {
+    console.log('identifier', identifier);
+    console.log('index', index);
+    const inputForEdit = document.getElementById('ident' + identifier)
+    const editBtn = document.getElementById('edit' + identifier)
+    const initialValue = document.getElementById('changedValue' + identifier)
+    const mainDivElement = document.getElementById('mainDiv'+identifier)
+    if (editBtn.innerHTML === 'Edit') {
+        editBtn.innerHTML = 'Save'
+        initialValue.classList.add('hideItem')
+        inputForEdit.classList.remove('hideItem')
+        console.log(mainDivElement.querySelectorAll("button"))
+
+    } else if (editBtn.innerHTML === 'Save') {
+        editBtn.innerHTML = 'Edit'
+        inputForEdit.classList.add('hideItem')
+        initialValue.classList.remove('hideItem')
+        initialValue.innerHTML = inputForEdit.value
+
+        initialTaskData.find((task, index) => initialTaskData[2].task = inputForEdit.value) // ცვლის ინფორმაციას ინფუთში და ობიექტში
+        initialTaskData.find((task, index) => console.log(index)) // konsoli
+
+        console.log(initialTaskData[1])
+        console.log(initialTaskData)
+        console.log(index)
+    }
+
+}
+
+// Item Removing
 function removeBtn(identifier) {
-
     initialTaskData = initialTaskData.filter((items) => {
-
 
         return items.id !== identifier
 
     })
-
-
     renderPagesData(initialTaskData)
 
+}
+
+function onCheckBox(identifier) {
+    const checkBox = document.getElementById('checkBox' + identifier)
+    const initialValue = document.getElementById('changedValue' + identifier)
+    if (checkBox.checked === true) {
+        initialValue.classList.add('done')
+    } else {
+        initialValue.classList.remove('done')
+
+    }
 
 }
